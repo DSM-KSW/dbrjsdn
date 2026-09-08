84c221ece99537cc8bb1cb42c299682cce925e52
suil0304 <eagle030410@gmail.com>
2026-09-08 14:16:46 +0900

    chore: 프로젝트 구조 설정

---
+++ main.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+def main(page:ft.Page):
+    # 여기에 코드 작성 바람
+    pass
+
+    # 보일러플레이트 코드
+    # page.title = "Flet App"
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    #
+    # text = ft.Text("Hello, Flet!", size=24, weight=ft.FontWeight.BOLD)
+    #
+    # page.add(text)
+
+if __name__ == "__main__":
+    ft.app(target=main)
